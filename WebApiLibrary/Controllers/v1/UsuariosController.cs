61c1e5800c7db4b33db0e7560ae28a04cded87ee
Eric <e.martin@estudiantes.matcom.uh.cu>
2022-11-03 19:43:56 -0400

    Fix Update user profile

--- WebApiLibrary/Controllers/v1/UsuariosController.cs
+++ WebApiLibrary/Controllers/v1/UsuariosController.cs
@@ -42,7 +42,7 @@ namespace WebApiLibrary.Controllers.v1
         }
 
         [HttpPut("{id:int}", Name = "updateUser")]
-        public async Task<ActionResult> Put(int id, [FromForm] UsuarioEdicionDTO userDTO)
+        public async Task<ActionResult> Put(int id, [FromBody] UsuarioEdicionDTO userDTO)
         {
             var userdb = await context.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
 
@@ -51,8 +51,8 @@ namespace WebApiLibrary.Controllers.v1
                 return NotFound();
             }
 
-            var user = mapper.Map<Usuario>(userDTO);
-            context.Entry(user).State = EntityState.Modified;
+            userdb = mapper.Map(userDTO, userdb);
+            context.Entry(userdb).State = EntityState.Modified;
             await context.SaveChangesAsync();
 
             return NoContent();
